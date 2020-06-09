269145c19a9cb7db3a7511ade26b69e91d3a85b3
Akira Kano <a.kano1101@gmail.com>
2020-06-09 19:40:03 +0900

    成分の型をdoubleからComponentTypeへ

--- include/mix/Vector.h
+++ include/mix/Vector.h
@@ -7,11 +7,11 @@
 namespace Mix
 {
 
-  struct Dimension {};
-  struct Dimension1D : Dimension { protected: double x_; };
-  struct Dimension2D : Dimension1D { protected: double y_; };
-  struct Dimension3D : Dimension2D { protected: double z_; };
-  struct Dimension4D : Dimension3D { protected: double w_; };
+  struct Dimension { public: typedef double ComponentType; };
+  struct Dimension1D : Dimension { protected: ComponentType x_; };
+  struct Dimension2D : Dimension1D { protected: ComponentType y_; };
+  struct Dimension3D : Dimension2D { protected: ComponentType z_; };
+  struct Dimension4D : Dimension3D { protected: ComponentType w_; };
 
   template<class>
   class Vector;
@@ -28,10 +28,12 @@ namespace Mix
 
   public:
 
-    double X() const { return this->x_; }
-    double Y() const { return this->y_; }
-    double Z() const { return this->z_; }
-    double W() const { return this->w_; }
+    typedef typename DimensionND::ComponentType ComponentType;
+
+    ComponentType X() const { return this->x_; }
+    ComponentType Y() const { return this->y_; }
+    ComponentType Z() const { return this->z_; }
+    ComponentType W() const { return this->w_; }
 
     Vector AsX() const { return AsX(Loki::Type2Type<DimensionND>()); }
     Vector AsY() const { return AsY(Loki::Type2Type<DimensionND>()); }
@@ -58,30 +60,30 @@ namespace Mix
     //Vector4D AsW(Loki::Type2Type<Dimension3D>) const { return Vector4D(this->x_, this->y_, this->z_, 0.0); }
     Vector4D AsW(Loki::Type2Type<Dimension4D>) const { return Vector4D(0.0, 0.0, 0.0, this->w_); }
 
-    explicit Vector(double x = 0.0, double y = 0.0, double z = 0.0, double t = 0.0)
+    explicit Vector(ComponentType x = 0.0, ComponentType y = 0.0, ComponentType z = 0.0, ComponentType t = 0.0)
     {
       Initialize(x, y, z, t, Loki::Type2Type<DimensionND>());
     }
 
-    void Initialize(double x, double y, double z, double t, Loki::Type2Type<Dimension1D>)
+    void Initialize(ComponentType x, ComponentType y, ComponentType z, ComponentType t, Loki::Type2Type<Dimension1D>)
     {
       this->x_ = x; UNUSED(y); UNUSED(z); UNUSED(t);
     }
-    void Initialize(double x, double y, double z, double t, Loki::Type2Type<Dimension2D>)
+    void Initialize(ComponentType x, ComponentType y, ComponentType z, ComponentType t, Loki::Type2Type<Dimension2D>)
     {
       this->x_ = x; this->y_ = y; UNUSED(z); UNUSED(t);
     }
-    void Initialize(double x, double y, double z, double t, Loki::Type2Type<Dimension3D>)
+    void Initialize(ComponentType x, ComponentType y, ComponentType z, ComponentType t, Loki::Type2Type<Dimension3D>)
     {
       this->x_ = x; this->y_ = y; this->z_ = z; UNUSED(t);
     }
-    void Initialize(double x, double y, double z, double t, Loki::Type2Type<Dimension4D>)
+    void Initialize(ComponentType x, ComponentType y, ComponentType z, ComponentType t, Loki::Type2Type<Dimension4D>)
     {
       this->x_ = x; this->y_ = y; this->z_ = z; this->w_ = t;
     }
 
-    void Reset(double x = 0.0, double y = 0.0, double z = 0.0, double t = 0.0) { this->Reset(Vector(x, y, z, t)); }
-    void Move(double x = 0.0, double y = 0.0, double z = 0.0, double t = 0.0) { this->Move(Vector(x, y, z, t)); }
+    void Reset(ComponentType x = 0.0, ComponentType y = 0.0, ComponentType z = 0.0, ComponentType t = 0.0) { this->Reset(Vector(x, y, z, t)); }
+    void Move(ComponentType x = 0.0, ComponentType y = 0.0, ComponentType z = 0.0, ComponentType t = 0.0) { this->Move(Vector(x, y, z, t)); }
 
     void Reset(const Vector& v) { this->Reset(v, Loki::Type2Type<DimensionND>()); }
     void Move(const Vector& v) { this->Move(v, Loki::Type2Type<DimensionND>()); }
@@ -118,24 +120,24 @@ namespace Mix
       this->x_ *= x ? -1 : 1; this->y_ *= y ? -1 : 1; this->z_ *= z ? -1 : 1; this->w_ *= t ? -1 : 1;
     }
 
-    double GetLength() const { return GetLength(Loki::Type2Type<DimensionND>()); }
+    ComponentType GetLength() const { return GetLength(Loki::Type2Type<DimensionND>()); }
     Vector GetInverse() const { return GetInverse(Loki::Type2Type<DimensionND>()); }
     Vector GetCompose(const Vector& v) const { return GetCompose(v, Loki::Type2Type<DimensionND>()); }
     Vector GetUnit() const { return GetUnit(GetLength(), Loki::Type2Type<DimensionND>()); }  //単位ベクトル生成
 
-    double GetLength(Loki::Type2Type<Dimension1D>) const
+    ComponentType GetLength(Loki::Type2Type<Dimension1D>) const
     {
       return abs(this->x_);
     }
-    double GetLength(Loki::Type2Type<Dimension2D>) const
+    ComponentType GetLength(Loki::Type2Type<Dimension2D>) const
     {
       return sqrt(this->x_ * this->x_ + this->y_ * this->y_);
     }
-    double GetLength(Loki::Type2Type<Dimension3D>) const
+    ComponentType GetLength(Loki::Type2Type<Dimension3D>) const
     {
       return sqrt(this->x_ * this->x_ + this->y_ * this->y_ + this->z_ * this->z_);
     }
-    double GetLength(Loki::Type2Type<Dimension4D>) const
+    ComponentType GetLength(Loki::Type2Type<Dimension4D>) const
     {
       return sqrt(this->x_ * this->x_ + this->y_ * this->y_ + this->z_ * this->z_ + this->w_ * this->w_);
     }
@@ -162,25 +164,25 @@ namespace Mix
       return Vector(this->x_ + v.x_, this->y_ + v.y_, this->z_ + v.z_, this->w_ + v.w_);
     }
 
-    Vector GetUnit(double len, Loki::Type2Type<Dimension1D>) const
+    Vector GetUnit(ComponentType len, Loki::Type2Type<Dimension1D>) const
     {
       return Vector(this->x_ / len);
     }
-    Vector GetUnit(double len, Loki::Type2Type<Dimension2D>) const
+    Vector GetUnit(ComponentType len, Loki::Type2Type<Dimension2D>) const
     {
       return Vector(this->x_ / len, this->y_ / len);
     }
-    Vector GetUnit(double len, Loki::Type2Type<Dimension3D>) const
+    Vector GetUnit(ComponentType len, Loki::Type2Type<Dimension3D>) const
     {
       return Vector(this->x_ / len, this->y_ / len, this->z_ / len);
     }
-    Vector GetUnit(double len, Loki::Type2Type<Dimension4D>) const
+    Vector GetUnit(ComponentType len, Loki::Type2Type<Dimension4D>) const
     {
       return Vector(this->x_ / len, this->y_ / len, this->z_ / len, this->w_ / len);
     }
 
-    double GetInner(const Vector2D & v) const { return (this->x_ * v.x_ + this->y_ * v.y_); }  //ベクトル内積計算
-    double GetCross(const Vector2D & v) const { return (this->x_ * v.y_ - v.x_ * this->y_); }  //ベクトル外積計算
+    ComponentType GetInner(const Vector2D & v) const { return (this->x_ * v.x_ + this->y_ * v.y_); }  //ベクトル内積計算
+    ComponentType GetCross(const Vector2D & v) const { return (this->x_ * v.y_ - v.x_ * this->y_); }  //ベクトル外積計算
 
     Vector1D As1D() const { return As1D(Loki::Type2Type<DimensionND>()); }
     Vector2D As2D() const { return As2D(Loki::Type2Type<DimensionND>()); }
